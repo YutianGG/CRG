a4a98078382a8cd2c8a1058db1e702065e22392c
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-21 12:46:41 +0800

    12/46 12.

---
+++ CRG/Assets/script/frooldown.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frooldown : MonoBehaviour
+{
+    public void froldo()
+    {
+        GetComponent<Animation>().Play();
+    }
+}
