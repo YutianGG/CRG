59392fe76b0e981b8186018a9b8b63739d7f6dcc
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-21 18:10:56 +0800

    5/21 18.

---
+++ CRG/Assets/script/player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 100)]
+    public float speed = 1;
+    [Header("跳躍高度"), Range(0, 10)]
+    public float jump = 2;
+    [Header("生命數量"), Range(1, 3)]
+    public int live = 3;
+    [Header("是否站在地板"), Tooltip("儲存玩家是否在地面")]
+    public bool isGround;
+    [Header("跳躍聲音")]
+    public AudioSource jumpaud;
+    [Header("死亡聲音")]
+    public AudioSource deadaud;
+
+    private int score;
+    private Rigidbody2D rig;
+    private AudioSource aud;
+    private Animator ani;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        aud = GetComponent<AudioSource>();
+        ani = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        
+    }
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizonatal");
+        rig.velocity = new Vector2(h * speed, rig.velocity.y);
+    }
+    private void Jump()
+    {
+        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        { 
+            
+        }
+    }
+}
