6f035a6492ae7fb567d6910a8570c1fb533615ec
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-06-11 12:58:03 +0800

    6/11 12.

--- CRG/Assets/script/playermovement.cs
+++ CRG/Assets/script/playermovement.cs
@@ -9,20 +9,20 @@ public class playermovement : MonoBehaviour
     public AudioSource jumpaud;
     [Header("死亡聲音")]
     public AudioSource deadaud;
+    [Header("分數")]
+    public int score;
+    public float speed,jumpForce;   //移動及跳躍參數
+    public Transform groundChenck;  //偵測物
+    public LayerMask ground;        //偵測物
+    public bool isGround, isJump;   //狀態判斷
+
     private Rigidbody2D rig;
     private Collider2D coll;
     private Animator anim;
-
-    private int score;
     private AudioSource aud;
-    public float speed,jumpForce;
-    public Transform groundChenck;
-    public LayerMask ground;
+    private bool jumpPressed;
+    private int jumpCount;
 
-    public bool isGround, isJump;
-
-    bool jumpPressed;
-    int jumpCount;
 
     // Start is called before the first frame update
     void Start()
@@ -42,43 +42,52 @@ public class playermovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        isGround = Physics2D.OverlapCircle(groundChenck.position, 0.1f, ground);
+        isGround = Physics2D.OverlapCircle(groundChenck.position, 0.1f, ground);    //在地面上
 
         GroundMovement();
         SwitchAnim();
         Jump();
     }
+    /// <summary>
+    /// 移動
+    /// </summary>
     private void GroundMovement()
     {
-        float horizontalMove = 1;
-        rig.velocity = new Vector2(horizontalMove * speed, rig.velocity.y);
+        float horizontalMove = 1;   //水平移動
+        rig.velocity = new Vector2(horizontalMove * speed, rig.velocity.y); //水平加速度
 
-        if (horizontalMove != 0)
+        if (horizontalMove != 0)    //如果水平移動不等於0
         {
-            transform.localScale = new  Vector3(horizontalMove, 1, 1);
+            transform.localScale = new  Vector3(horizontalMove, 1, 1);  //判斷方向面向
         }
     }
+    /// <summary>
+    /// 跳躍及判斷
+    /// </summary>
     private void Jump()
     {
-        if (isGround)
+        if (isGround)   //如果在地面
         {
-            jumpCount = 2;
-            isJump = false;
+            jumpCount = 2;  //可連跳躍次數
+            isJump = false; //跳躍動畫 = false
         }
-        if (jumpPressed && isGround)
+        if (jumpPressed && isGround)    //如果按下跳躍鍵且在地面上
         {
-            isJump = true;
-            rig.velocity = new Vector2(rig.velocity.x, jumpForce);
-            jumpCount--;
-            jumpPressed = false;
+            isJump = true;  //跳躍動畫 = true
+            rig.velocity = new Vector2(rig.velocity.x, jumpForce);  //跳躍參數
+            jumpCount--;    //跳躍次數-1
+            jumpPressed = false;    //確保if迴圈執行完
         }
-        else if (jumpPressed && jumpCount > 0 && isJump )
+        else if (jumpPressed && jumpCount > 0 && isJump )   //如果按下跳躍鍵&&跳躍次數大於0&&跳躍動畫執行
         {
-            rig.velocity = new Vector2(rig.velocity.x, jumpForce);
-            jumpCount--;
-            jumpPressed = false;
+            rig.velocity = new Vector2(rig.velocity.x, jumpForce);  //跳躍參數
+            jumpCount--;    //跳躍次數-1
+            jumpPressed = false;    //確保完成else if迴圈
         }
     }
+    /// <summary>
+    /// 動畫判斷
+    /// </summary>
     private void SwitchAnim()
     {
         anim.SetBool("run", true);
@@ -89,6 +98,7 @@ public class playermovement : MonoBehaviour
         else if (!isGround && rig.velocity.y > 0)
         {
             anim.SetBool("jump", true);
+            anim.SetBool("fall", false);
         }
         else if (rig.velocity.y < 0)
         {
@@ -96,11 +106,15 @@ public class playermovement : MonoBehaviour
             anim.SetBool("fall", true);
         }
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    /// <summary>
+    /// 地面物件刪除(碰到物件)
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionExit2D(Collision2D collision)   
     {
-        if (collision.gameObject.tag == "floor")
+        if (collision.gameObject.tag == "floor")    //完加觸碰到的圖層為floor
         {
-            Destroy(collision.gameObject, 0.5f);
+            Destroy(collision.gameObject, 0.5f);    //消除所碰到之物件.5秒鐘刪除
         }
     }
 }
